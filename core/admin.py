6ecb182478c13d6a7c2e9fdc4ec2035bc9e20c5e
BeepBoopYeetYourScoot <zdmitry@alabuga.ru>
2021-01-27 17:15:56 +0300

    Добавил модель таймера, накидал обработчик

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MainUser, GameSetting, Transaction, Session, Player, Production, Turn, Warehouse, UserWebSocket
+from .models import MainUser, TurnCountdownTimer, GameSetting, Transaction, Session, Player, Production, Turn, Warehouse, UserWebSocket
 
 admin.site.register(MainUser)
 admin.site.register(GameSetting)
@@ -11,4 +11,5 @@ admin.site.register(Production)
 admin.site.register(Turn)
 admin.site.register(Warehouse)
 admin.site.register(UserWebSocket)
+admin.site.register(TurnCountdownTimer)
 
--- core/models.py
+++ core/models.py
@@ -4,6 +4,8 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from .services.broker_services import count_brokers
 from .services.manufacturer_services import count_manufacturers
+from countdowntimer_model.models import CountdownTimer
+from .services.timer import set_turn_timers
 
 
 def send_sok_get_games():
@@ -15,6 +17,7 @@ def send_sok_change_session_id(session_id):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(f"session_{session_id}",
                                             {"type": "send_detail_data", 'pk': f'{session_id}'})
+
 
 CITIES = (
     ('NF', "Неверфол"),
@@ -40,19 +43,6 @@ ROLES = (
     (MANUFACTURER, 'Производитель'),
 )
 
-turn_time_default = {
-    1: 15,
-    2: 10,
-    3: 10,
-    4: 10,
-    5: 7,
-    6: 7,
-    7: 7,
-    8: 7,
-    9: 7,
-    10: 7
-}
-
 
 class MainUser(AbstractUser):
     """Модель пользователя платформы"""
@@ -62,6 +52,11 @@ class MainUser(AbstractUser):
         verbose_name_plural = 'Пользователи'
 
 
+class TurnCountdownTimer(CountdownTimer):
+    def __str__(self):
+        return f'Таймер {self.pk}'
+
+
 class GameSetting(models.Model):
     """Модель пресета настроек игры"""
     manufacturer_balance = models.PositiveIntegerField(verbose_name='Баланс производителя')
@@ -69,7 +64,6 @@ class GameSetting(models.Model):
     crown_balance = models.PositiveIntegerField(verbose_name="Баланс короны", default=12000)
     transaction_limit = models.PositiveIntegerField(verbose_name="Лимит сделки")
     number_of_brokers = models.PositiveIntegerField(verbose_name='Количество маклеров в сессии', default=3)
-    turn_time_preset = models.TextField(default=turn_time_default)  # JSONField; ячейка должна хранить словарь с временами ходов
 
     def __str__(self):
         return f'Сет настроек {self.pk}'
@@ -84,10 +78,11 @@ class Session(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Название сессии')
     turn_count = models.PositiveIntegerField(verbose_name='Количество игровых ходов')
-    # FIXME Не работает, как надо:
-    #  Нужно, чтобы при создании сесии
-    settings = models.OneToOneField(GameSetting, related_name='session', on_delete=models.SET_NULL, null=True,
-                                 default=None)
+    settings = models.OneToOneField(GameSetting,
+                                    related_name='session',
+                                    on_delete=models.SET_NULL,
+                                    null=True,
+                                    default=None)
     status = models.CharField(max_length=100, choices=SESSION_STATUS, verbose_name='Статус сессии', default='Created')
     is_started = models.BooleanField(default=False)
     player_count = models.IntegerField(verbose_name="количество игроков", default=14)
@@ -194,7 +189,7 @@ class Production(models.Model):
 
 class Warehouse(models.Model):
     """Модель склада производителей"""
-    player = models.ForeignKey(Player,
+    player = models.OneToOneField(Player,
                                on_delete=models.SET_NULL,
                                null=True,
                                related_name='warehouse',
@@ -210,6 +205,10 @@ class Turn(models.Model):
     turn_num = models.IntegerField(verbose_name='Номер хода')
     session = models.ForeignKey(Session, verbose_name='Сессия', on_delete=models.CASCADE, related_name='turn')
     turn_time = models.PositiveIntegerField(verbose_name='Время хода', blank=True, default=30)
+    timer = models.OneToOneField(TurnCountdownTimer, verbose_name='Таймер хода',
+                                 on_delete=models.SET_NULL,
+                                 related_name='turn',
+                                 null=True)
 
     # FIXME состакать с таймером
     turn_finished = models.BooleanField(default=False)
